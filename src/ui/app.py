2dd18f313a2bee3ebfa747386dbdd9c970fa7d02
Davide Mastricci <davide.mastricci7@gmai.com>
2024-07-31 18:51:19 +0200

    adding autocaption service and adding ui for it ttoo

--- src/ui/app.py
+++ src/ui/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from PIL import Image
 from src.services.aesthetic_predictor import predict
+from src.services.autocaption import process_image
 
 st.header("3Sides Image Aesthetics Scorer")
 
@@ -20,10 +21,20 @@ if uploaded_files:
 
     images_and_scores.sort(key=lambda x: x[1], reverse=True)
 
+    if "results" not in st.session_state:
+        st.session_state["results"] = {}
     cols = st.columns(len(images_and_scores))
 
     for col, (image, score, filename) in zip(cols, images_and_scores):
         with col:
             st.image(image, caption=f"{filename} - Score: {score}", width=100)
+            if st.button(f"Process {filename}"):
+                with st.spinner(f"AI is generating content for {filename}..."):
+                    result = process_image(image)
+                    st.session_state.results[filename] = result
+
+            # Display the result if it exists in the session state
+            if filename in st.session_state.results:
+                st.write(f"{st.session_state.results[filename]}")
 else:
     st.warning("No images uploaded. Please upload images.")
